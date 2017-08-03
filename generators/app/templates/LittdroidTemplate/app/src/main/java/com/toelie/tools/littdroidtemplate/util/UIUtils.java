35cf47bd9a68f434f493cd17f08cc30d717aa6d6
toe-lie <toelie.cumlm.cs@gmail.com>
2017-08-03 12:20:06 +0630

    [add] Add UIUtils class

---
+++ generators/app/templates/LittdroidTemplate/app/src/main/java/com/toelie/tools/littdroidtemplate/util/UIUtils.java
@@ -0,0 +1,15 @@
+package com.toelie.tools.littdroidtemplate.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Created by toe-lie on 6/15/2017.
+ */
+
+public class UIUtils {
+
+    public static int dpToPx(Context context, int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+}
