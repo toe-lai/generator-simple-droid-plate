3655d9cc8f60fdabfe0acedec9347919a16b31b3
toe-lai <toelai.tl@gmail.com>
2017-09-22 14:04:28 +0630

    [update] Update gradle version and fix test_act.xml "java.lang.UnsupportedOperationException: Can't convert to dimension: type=0x1" issue, it's cause because of using missing dimen.

--- generators/app/templates/LittdroidTemplate/app/src/main/java/com/toelie/tools/littdroidtemplate/TestActivity.java
+++ generators/app/templates/LittdroidTemplate/app/src/main/java/com/toelie/tools/littdroidtemplate/TestActivity.java
@@ -3,8 +3,6 @@ package com.toelie.tools.littdroidtemplate;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -15,7 +13,7 @@ public class TestActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
   }
-
+/*
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
@@ -36,5 +34,5 @@ public class TestActivity extends AppCompatActivity {
     }
 
     return super.onOptionsItemSelected(item);
-  }
+  }*/
 }
